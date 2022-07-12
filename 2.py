43854c7282ac5be035cee4b34bdb3185831aff3c
furkan <furkanyildiz0660@gmail.com>
2022-07-12 16:46:55 +0300

    aciklama satirlari eklendi

--- 2.py
+++ 2.py
@@ -1,3 +1,14 @@
+"""2. Sorunnun Açıklaması:
+
+Verilen listenin içindeki elemanları tersine döndüren bir fonksiyon yazın. Eğer listenin içindeki elemanlar da liste içeriyorsa onların elemanlarını da tersine döndürün. Örnek olarak:
+
+input: [[1, 2], [3, 4], [5, 6, 7]]
+
+output: [[[7, 6, 5], [4, 3], [2, 1]]"""
+
+
+
+
 l1 = [[1, 2], [3, 4], [5, 6, 7]]
 
 l2 = l1[::-1]
